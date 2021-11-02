c8811171be1e9b0f4f24b89cb2a42aea8c0526ff
danielabbott <dabbott@live.co.uk>
2021-11-02 20:21:16 +0000

    Fix Windows build

--- BuildAssets.bat
+++ BuildAssets.bat
@@ -34,6 +34,7 @@ copy standard_assets\shaders\ubo.glsl build\standard_assets\shaders\ubo.glsl
 
 glslc -O -DNDEBUG standard_assets\shaders\downscale.frag -o build\release\standard_assets\shaders\downscale.frag.spv
 glslc -O -DNDEBUG standard_assets\shaders\ssao.frag -o build\release\standard_assets\shaders\ssao.frag.spv
+glslc -O -DNDEBUG standard_assets\shaders\downscale_ssao.frag -o build\release\standard_assets\shaders\downscale_ssao.frag.spv
 glslc -O -DNDEBUG standard_assets\shaders\kawase_ssao.frag -o build\release\standard_assets\shaders\kawase_ssao.frag.spv
 glslc -O -DNDEBUG standard_assets\shaders\kawase_rgb.frag -o build\release\standard_assets\shaders\kawase_rgb.frag.spv
 glslc -O -DNDEBUG standard_assets\shaders\kawase_merge.frag -o build\release\standard_assets\shaders\kawase_merge.frag.spv
@@ -54,26 +55,27 @@ glslc -O -DNDEBUG -DSKINNED -DOBJECT_DEPTH standard_assets\shaders\object.vert -
 glslc -O -DNDEBUG -DSKINNED -DOBJECT_DEPTH_ALPHA standard_assets\shaders\object.vert -o build\release\standard_assets\shaders\object.vert.skinned.depth_alpha.spv
 
 
-glslc -O -DDEBUG standard_assets\shaders\downscale.frag -o build\release\standard_assets\shaders\downscale.frag.spv
-glslc -O -DDEBUG standard_assets\shaders\ssao.frag -o build\release\standard_assets\shaders\ssao.frag.spv
-glslc -O -DDEBUG standard_assets\shaders\kawase_ssao.frag -o build\release\standard_assets\shaders\kawase_ssao.frag.spv
-glslc -O -DDEBUG standard_assets\shaders\kawase_rgb.frag -o build\release\standard_assets\shaders\kawase_rgb.frag.spv
-glslc -O -DDEBUG standard_assets\shaders\kawase_merge.frag -o build\release\standard_assets\shaders\kawase_merge.frag.spv
-glslc -O -DDEBUG standard_assets\shaders\object.frag -o build\release\standard_assets\shaders\object.frag.spv
-glslc -O -DDEBUG standard_assets\shaders\object_depth_alpha.frag -o build\release\standard_assets\shaders\object_depth_alpha.frag.spv
-glslc -O -DDEBUG standard_assets\shaders\post.frag -o build\release\standard_assets\shaders\post.frag.spv
-glslc -O -DDEBUG standard_assets\shaders\skybox.frag -o build\release\standard_assets\shaders\skybox.frag.spv
+glslc -O -DDEBUG standard_assets\shaders\downscale.frag -o build\debug\standard_assets\shaders\downscale.frag.spv
+glslc -O -DDEBUG standard_assets\shaders\ssao.frag -o build\debug\standard_assets\shaders\ssao.frag.spv
+glslc -O -DNDEBUG standard_assets\shaders\downscale_ssao.frag -o build\debug\standard_assets\shaders\downscale_ssao.frag.spv
+glslc -O -DDEBUG standard_assets\shaders\kawase_ssao.frag -o build\debug\standard_assets\shaders\kawase_ssao.frag.spv
+glslc -O -DDEBUG standard_assets\shaders\kawase_rgb.frag -o build\debug\standard_assets\shaders\kawase_rgb.frag.spv
+glslc -O -DDEBUG standard_assets\shaders\kawase_merge.frag -o build\debug\standard_assets\shaders\kawase_merge.frag.spv
+glslc -O -DDEBUG standard_assets\shaders\object.frag -o build\debug\standard_assets\shaders\object.frag.spv
+glslc -O -DDEBUG standard_assets\shaders\object_depth_alpha.frag -o build\debug\standard_assets\shaders\object_depth_alpha.frag.spv
+glslc -O -DDEBUG standard_assets\shaders\post.frag -o build\debug\standard_assets\shaders\post.frag.spv
+glslc -O -DDEBUG standard_assets\shaders\skybox.frag -o build\debug\standard_assets\shaders\skybox.frag.spv
 
-glslc -O -DDEBUG standard_assets\shaders\fullscreen.vert -o build\release\standard_assets\shaders\fullscreen.vert.spv
-glslc -O -DDEBUG standard_assets\shaders\post.vert -o build\release\standard_assets\shaders\post.vert.spv
-glslc -O -DDEBUG standard_assets\shaders\skybox.vert -o build\release\standard_assets\shaders\skybox.vert.spv
+glslc -O -DDEBUG standard_assets\shaders\fullscreen.vert -o build\debug\standard_assets\shaders\fullscreen.vert.spv
+glslc -O -DDEBUG standard_assets\shaders\post.vert -o build\debug\standard_assets\shaders\post.vert.spv
+glslc -O -DDEBUG standard_assets\shaders\skybox.vert -o build\debug\standard_assets\shaders\skybox.vert.spv
 
-glslc -O -DDEBUG standard_assets\shaders\object.vert -o build\release\standard_assets\shaders\object.vert.spv
-glslc -O -DDEBUG -DOBJECT_DEPTH standard_assets\shaders\object.vert -o build\release\standard_assets\shaders\object.vert.depth.spv
-glslc -O -DDEBUG -DOBJECT_DEPTH_ALPHA standard_assets\shaders\object.vert -o build\release\standard_assets\shaders\object.vert.depth_alpha.spv
-glslc -O -DDEBUG -DSKINNED standard_assets\shaders\object.vert -o build\release\standard_assets\shaders\object.vert.skinned.spv
-glslc -O -DDEBUG -DSKINNED -DOBJECT_DEPTH standard_assets\shaders\object.vert -o build\release\standard_assets\shaders\object.vert.skinned.depth.spv
-glslc -O -DDEBUG -DSKINNED -DOBJECT_DEPTH_ALPHA standard_assets\shaders\object.vert -o build\release\standard_assets\shaders\object.vert.skinned.depth_alpha.spv
+glslc -O -DDEBUG standard_assets\shaders\object.vert -o build\debug\standard_assets\shaders\object.vert.spv
+glslc -O -DDEBUG -DOBJECT_DEPTH standard_assets\shaders\object.vert -o build\debug\standard_assets\shaders\object.vert.depth.spv
+glslc -O -DDEBUG -DOBJECT_DEPTH_ALPHA standard_assets\shaders\object.vert -o build\debug\standard_assets\shaders\object.vert.depth_alpha.spv
+glslc -O -DDEBUG -DSKINNED standard_assets\shaders\object.vert -o build\debug\standard_assets\shaders\object.vert.skinned.spv
+glslc -O -DDEBUG -DSKINNED -DOBJECT_DEPTH standard_assets\shaders\object.vert -o build\debug\standard_assets\shaders\object.vert.skinned.depth.spv
+glslc -O -DDEBUG -DSKINNED -DOBJECT_DEPTH_ALPHA standard_assets\shaders\object.vert -o build\debug\standard_assets\shaders\object.vert.skinned.depth_alpha.spv
 
 
 
